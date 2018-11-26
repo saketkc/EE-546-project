809f1f44095fc11f80f642eb6d695c3a1788287c
Saket Choudhary <saketkc@gmail.com>
2018-11-25 19:08:50 -0800

    add py

---
+++ notebooks/ee546_utils.py
@@ -0,0 +1,156 @@
+import glob
+import spams
+import pandas as pd
+from numpy.linalg import pinv as moore_penrose_inv
+from numpy.linalg import svd, eig
+from tqdm import tqdm
+import seaborn as sns
+def SVD_impute(df):
+    """Perform SVD imputing.
+    
+    
+    Parameters
+    ----------
+    matrix: array_like
+            Array with rows as genes and columns as tissues/samples
+            with nans for missing values
+    
+    Returns
+    -------
+    filled_matrix: array_like
+                   Imputed matrix with entries filled by relying on eigengenes    
+    """
+    
+    null_idx = pd.isnull(df).any(1).nonzero()[0]
+
+    full_matrix = df.values
+    non_null_matrix  = df.dropna().values
+    U, S, V_T = np.linalg.svd(non_null_matrix)
+    V =  V_T.T
+    
+    for index in null_idx:
+        x = full_matrix[index,:]
+
+        # index of entries where the row is nan
+        row_na_idx = np.where(np.isnan(x))
+        
+        # index of entries where the row is not nan        
+        row_not_na_idx = np.where(~np.isnan(x))
+        
+        
+        # entries from row which are not nan        
+        x_star_not_na = x[row_not_na_idx]
+        
+         # entries from row which are not nan        
+        x_star_na = x[row_na_idx]
+
+        # Eigen genes corresponding to index 
+        # of locations (columns) in row x which are nan
+        V_star_na = np.delete(V, row_not_na_idx, 0)
+        
+        
+        # Delete rows from V, corresponding to index 
+        # of locations (columns) in row x which are nan
+        # We will use these to project the row with complete
+        V_star_not_na = np.delete(V, row_na_idx, 0)
+
+
+        gram = np.dot(V_star_not_na.T, V_star_not_na)
+        coeff = np.dot(np.linalg.pinv(gram), V_star_not_na.T)
+
+        beta = np.dot(coeff, x_star_not_na)
+        
+        # xhat = V_J \beta
+        x_missing = np.dot(V_star_na, beta)
+        
+        # Fill in the missing entries
+        full_matrix[index, row_na_idx] = x_missing
+    return full_matrix
+
+def plot_SVD_utils(U, S, VT):
+    V = VT.T
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(30, 20))
+    ax = axes[0, 0]
+    sns.heatmap(V, square=True, cmap='RdBu', ax=ax, cbar=False)
+    
+    ax = axes[0, 1]
+    sns.barplot(x=np.arange(1, len(S)+1), y=S**2/np.sum(S**2), ax=ax, color=sns.color_palette('Set2')[1])
+    
+    ax = axes[0, 2]
+    ax.plot(V[:, 0], marker='o')
+    
+    ax = axes[1, 0]
+    ax.plot(V[:, 1], marker='o')
+    
+    ax = axes[1, 1]
+    ax.plot(V[:, 2], marker='o')
+    
+    ax = axes[1, 2]
+    ax.plot(V[:, 3], marker='o')
+    
+    
+def einsum_row_norm(a):
+    return np.sqrt(np.einsum('ij,ij->i', a, a))
+
+def einsum_col_norm(a):
+    return np.sqrt(np.einsum('ij,ij->j', a, a))
+
+
+def ho_SVD(list_of_X):
+    """ Perform higher order SVD
+    
+    list_of_X: list
+               [X_1, X_2, X_3, ..., X_n] size (n x m)
+    
+    """
+    # Step 1
+    # A_i = X_i^T X_i
+    # A_inv = moore_penrose_inv(A_i)
+    A = []
+    N = len(list_of_X)
+    A_inv = []
+    for X in list_of_X:
+        A_i = np.dot(X.T, X)
+        A.append(A_i)
+        A_inv.append(np.linalg.inv((A_i)))
+    # Step 2
+    # W = 1/N(N-1) * sum_{i=1}^jn sum_{j>i}^N A_i moore_penrose_inv(A_j) + A_j moore_penrose_inv(A_i)
+    W = 0
+    
+    for i in range(N):
+        for j in range(i+1, N):
+            W = W + (np.dot(A[i], A_inv[j]) + np.dot(A[j], A_inv[i]))
+    W =  W/(N*(N-1))
+    
+    # Step 3
+    # Eigen decomposition of W?
+    # WV = V\Lambda
+    # V= (v_1, v_2, ..., v_n)
+    # ||v||_k =1, but V is not necesarrily orthonormal
+    # But we don't
+    
+    eigen_values, V = eig(W)
+
+    idx = eigen_values.argsort()[::-1]   
+    eigen_values = eigen_values[idx]
+    V = V[:,idx]
+    
+    
+    # Step 4
+    # solve for  VB_i = X_i^T
+     #Calculate the Bi matrices
+   
+    B = []
+    U = []
+    sigmas = []
+    for X in list_of_X:
+        #B_i = spams.omp( np.asfortranarray(X.T), V, lambda1=0, numThreads = 24)]
+        # VB.T = X.T
+        B_i = np.dot(np.linalg.inv(V), X.T).T
+        B.append(B_i)
+        sigma_i  = np.linalg.norm(B_i, axis=0) #einsum_col_norm(B)
+        U_i = B_i/sigma_i 
+        sigmas.append(sigma_i)
+        U.append(U_i)
+    return U, sigmas, V
+    
