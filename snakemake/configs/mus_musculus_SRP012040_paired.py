e9c1a9725940d4fb3786854ac0d2f8dadda049ad
Saket Choudhary <saketkc@gmail.com>
2018-11-29 16:22:14 -0800

    add config

--- snakemake/configs/mus_musculus_SRP012040_paired.py
+++ snakemake/configs/mus_musculus_SRP012040_paired.py
@@ -1,5 +1,5 @@
 
 RAWDATA_DIR = '/staging/as/skchoudh/rna-seq-datasets/paired/mus_musculus/SRP012040'
-OUT_DIR = '/staging/as/skchoudh/re-ribo-analysis/mus_musculus/SRP012040'
-CDNA_FA_GZ = '/home/cmb-panasas2/skchoudh/genomes/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz'    
-CDNA_IDX = '/home/cmb-panasas2/skchoudh/genomes/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.kallisto.index'    
+OUT_DIR = '/staging/as/skchoudh/rna-seq-output/mus_musculus/SRP012040'
+CDNA_FA_GZ = '/home/cmb-panasas2/skchoudh/genomes/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.fa.gz'
+CDNA_IDX = '/home/cmb-panasas2/skchoudh/genomes/mus_musculus/cdna/Mus_musculus.GRCm38.cdna.all.kallisto.index'
